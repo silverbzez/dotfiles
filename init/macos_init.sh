5f2ab3620edf208be80a6a6f05c6a2580bf7b532
Even Zhou <silverbz.dev@outlook.com>
2022-11-25 10:43:54 -0500

    update on Nov 25, 2022

--- init/macos_init.sh
+++ init/macos_init.sh
@@ -10,15 +10,16 @@ printf "Please manually download MacPorts in the following link:\n"
 
 sudo /opt/local/bin/port selfupdate
 sudo /opt/local/bin/port -N install bash bash-completion vim git wget curl tree openssh podman
+sudo /opt/local/bin/port -N install python38 py38-pip py38-setuptools py38-autopep8 py38-codestyle
+sudo /opt/local/bin/port -N install python39 py39-pip py39-setuptools py39-autopep8 py39-codestyle
 sudo /opt/local/bin/port -N install python310 py310-pip py310-setuptools py310-autopep8 py310-codestyle
-sudo /opt/local/bin/port -N install python311 py311-pip py311-setuptools py311-autopep8 py311-codestyle
 
-sudo /opt/local/bin/port select --set python python311
-sudo /opt/local/bin/port select --set python3 python311
-sudo /opt/local/bin/port select --set pip pip311
-sudo /opt/local/bin/port select --set pip3 pip311
-sudo /opt/local/bin/port select --set autopep8 autopep8-311
-sudo /opt/local/bin/port select --set pycodestyle pycodestyle-py311
+sudo /opt/local/bin/port select --set python python310
+sudo /opt/local/bin/port select --set python3 python310
+sudo /opt/local/bin/port select --set pip pip310
+sudo /opt/local/bin/port select --set pip3 pip310
+sudo /opt/local/bin/port select --set autopep8 autopep8-310
+sudo /opt/local/bin/port select --set pycodestyle pycodestyle-py310
 
 printf "\e[36mInstalling Homebrew\e[m\n"
 
@@ -26,7 +27,10 @@ printf "\e[36mInstalling Homebrew\e[m\n"
 
 /opt/homebrew/bin/brew tap homebrew/cask-fonts
 /opt/homebrew/bin/brew update
-/opt/homebrew/bin/brew install --cask appcleaner the-unarchiver visual-studio-code iterm2 google-chrome microsoft-edge steam-plus-plus steam mactex-no-gui
+/opt/homebrew/bin/brew install --cask google-chrome microsoft-edge
+/opt/homebrew/bin/brew install --cask appcleaner the-unarchiver iina
+/opt/homebrew/bin/brew install --cask visual-studio-code iterm2 github mactex-no-gui
+/opt/homebrew/bin/brew install --cask steam-plus-plus steam
 /opt/homebrew/bin/brew install --cask font-cascadia-code font-cascadia-code-pl
 
 ### SMB Sharing on macOS
